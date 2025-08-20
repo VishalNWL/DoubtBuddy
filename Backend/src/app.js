2bfe3021dcd6111372b1e54391f73edce2262c49
VishalNWL <nrsvishaln@gmail.com>
2025-08-20 19:54:33 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -31,6 +31,7 @@ const corsOptions = {
 app.options("*", cors(corsOptions));
 app.use(cors(corsOptions));
 
+app.options("*", cors({ origin: /\.vercel\.app$/, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("Public"));
